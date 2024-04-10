4f3a7dd0015128359b074823567222b5832edff9
Aryan-219 <aryanagr219@gmail.com>
2024-04-10 13:17:32 +0530

    SetMismatch done

---
+++ DSA with Java/FirstMissingPositive.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/first-missing-positive/description/
+
+import java.util.Arrays;
+
+class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums = { 7,8,9,11,12};
+        // int[] nums = {1};
+        // int[] nums = {1,2,0};
+
+        System.out.println(firstMissingPositive(nums));
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        // System.out.println(Arrays.toString(nums));
+        cycleSort(nums);
+        // System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i+1 ) {
+                return i+1 ;
+            }
+        }
+        return nums.length+1 ;
+    }
+
+    public static int[] cycleSort(int[] nums) {
+
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i]-1;
+
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        return nums;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
--- DSA with Java/SetMismatch.java
+++ DSA with Java/SetMismatch.java
@@ -1,20 +1,18 @@
-
-// https://leetcode.com/problems/find-all-duplicates-in-an-array/
-import java.util.List;
-import java.util.ArrayList;
+// https://leetcode.com/problems/set-mismatch/description/
 
 class Solution {
-    public List<Integer> findDuplicates(int[] nums) {
+    public int[] findErrorNums(int[] nums) {
         cycleSort(nums);
-
-        List<Integer> result = new ArrayList<>();
+        int[] result = new int[2];
         for(int i=0; i <= nums.length-1; i++){
             if(nums[i]!= i+1){
-                result.add(nums[i]);
+                result[0] = nums[i];
+                result[1] = i+1;
             }
         }
         return result;
     }
+
     public static int[] cycleSort(int[] arr) {
         int i = 0;
         while (i < arr.length) {
@@ -34,8 +32,3 @@ class Solution {
         arr[second] = temp;
     }
 }
-
-// A general thumb rule we must follow :
-// If we are asked to find "All" like all duplicates, or all missing,
-// Then first apply cycle sort then apply a for loop to find all duplicates or all missing
-// But if we are asked to find just a single duplicate or a single missing number, then try to find the answer in the sorting loop itself.

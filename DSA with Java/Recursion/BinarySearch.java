caa21479ff62cfac3f90f86cfb562d3a69dd91c9
Aryan-219 <aryanagr219@gmail.com>
2024-05-30 13:19:02 +0530

    uploaded the gfg pdf

--- DSA with Java/Recursion/BinarySearch.java
+++ DSA with Java/Recursion/BinarySearch.java
@@ -1,7 +1,7 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr =  { 1, 2, 3, 4, 55,66,78 };
-        int target = 100;
+        int target = 2;
         System.out.println(binarySearch(arr, target, 0, arr.length-1));
     }
 
@@ -14,7 +14,7 @@ public class BinarySearch {
             return mid;
         }
         if (target<arr[mid] ) {
-            return binarySearch(arr, target, s, mid - 1);
+             binarySearch(arr, target, s, mid - 1);
         }
         return binarySearch(arr, target, mid + 1, e);
     }

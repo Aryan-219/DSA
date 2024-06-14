f33db2ae8f3dad9e145d840a01ace8d49d59336c
Aryan-219 <aryanagr219@gmail.com>
2024-06-14 22:55:53 +0530

    Fibonacci formula

--- DSA with Java/Recursion/Fibonacci.java
+++ DSA with Java/Recursion/Fibonacci.java
@@ -1,12 +1,17 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibonacci(7));
+        System.out.println(fiboFormula(50));
     }
 
-    public static int fibonacci(int n){
-        if(n<2){
+    static int fiboFormula(int n){
+        // Just for demo, use long instead
+        return (int)(Math.pow(((1+Math.sqrt(5))/2),n)/ Math.sqrt(5));
+    }
+
+    public static int fibonacci(int n) {
+        if (n < 2) {
             return n;
         }
-        return fibonacci(n-1) + fibonacci(n-2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
